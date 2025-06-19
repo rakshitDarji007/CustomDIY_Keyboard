a908085b5aa68df070dbc62c5117bb7e05479fd8
rakshitDarji007 <rakshit.darji007@gmail.com>
2025-06-19 00:35:21 -0500

    imported all the neccessay libraries

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,1 +1,14 @@
-print("Hello World!!")
+import time
+import board
+import busio
+import digitalio
+import rotaryio
+import usb_hid
+import adafruit_matrixkeypad
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+import adafruit_ssd1306
+from adafruit_display_text import label
+import terminalio
